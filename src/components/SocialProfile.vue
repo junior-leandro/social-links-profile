a734e277798a5588e6fafbf6c86d8cc4722fcef2
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-02-10 16:42:45 -0300

    layout update

--- src/components/SocialProfile.vue
+++ src/components/SocialProfile.vue
@@ -1,9 +1,11 @@
  
 <template>
     <div class="social-profile flex">
-    <img src="../assets/avatar-jessica.jpeg" alt="Avatar Jessica" class="avatar">
-    <span>Jessica Randall</span>
-    <span>London, United Kingdom</span>
-    <span>"Front-end developer and avid reader."</span>
+        <img src="../assets/avatar-jessica.jpeg" alt="Avatar Jessica" class="avatar">
+        <div class="flex user-data">
+            <span class="user-name">Jessica Randall</span>
+            <span class="user-adress">London, United Kingdom</span>
+        </div>
+        <span class="user-message">"Front-end developer and avid reader."</span>
     </div>
 </template>
